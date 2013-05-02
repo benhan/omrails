c86ea96271820d6dbbee7c97a46796cfb31ece1c
Ben Han <benhan2@gmail.com>
2013-05-01 21:56:28 -0400

    Add endless scroll to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	@pins = @user.pins.order("created_at desc").page(params[:page]).per_page(20)
   end
+
+  respond_to do |format|
+    format.html # index.html.erb
+    format.json { render json: @user }
+    format.js
+  end
 end
